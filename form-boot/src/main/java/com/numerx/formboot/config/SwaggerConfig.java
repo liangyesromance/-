c110f8184481768c9ca893a94f6b8b8c8c2ec362
liangyesromance <95199228+liangyesromance@users.noreply.github.com>
2022-06-15 14:59:59 +0800

    Initialize dependabot/npm_and_yarn/form-front/form-design/shelljs-0.8.5

---
+++ form-boot/src/main/java/com/numerx/formboot/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.numerx.formboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author matrixorz
+ * @data 2019-08-13 23:16
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                //.apis(RequestHandlerSelectors.basePackage("com.vacomall.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("restful-api,Restfull接口生成框架")
+                .termsOfServiceUrl("http://www.justpic.org/")
+                .description("springmvc + swagger2 Restfull接口生成框架")
+                .contact(new Contact("numerx", "http://www.justpic.org/", "i@justpic.org"))
+                .version("1.0.0")
+                .build();
+    }
+}
